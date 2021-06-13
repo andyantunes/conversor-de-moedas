c7504ce47565814c69a24310052971cbadaff787
anderson.antunes <andersonlantunes@gmail.com>
2021-06-13 16:04:35 -0300

    Implementing number validation directive

--- src/app/conversor/directives/numero.directive.ts
+++ src/app/conversor/directives/numero.directive.ts
@@ -1,11 +1,21 @@
 import { Directive, HostListener, ElementRef } from '@angular/core';
-import {} from '@angular/forms';
+import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
 
 @Directive({
   selector: '[numero]',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: NumeroDirective,
+      multi: true,
+    },
+  ],
 })
-export class NumeroDirective {
-  constructor() {}
+export class NumeroDirective implements ControlValueAccessor {
+  onTouched: any;
+  onChange: any;
+
+  constructor(private el: ElementRef) {}
 
   /**
    * Implementa evento de keyup para o elemento da diretiva.
@@ -23,6 +33,34 @@ export class NumeroDirective {
     }
 
     $event.target.value = valor;
-    // this.onChange(valor);
+    this.onChange(valor);
+  }
+
+  /**
+   * Registra a função a ser chamada para atualizar o valor na model.
+   *
+   * @param fn any
+   */
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  /**
+   * Registra a função a ser chamada para atualizar o valor na model
+   * para o evento touched.
+   *
+   * @param fn any
+   */
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  /**
+   * Obtém o valor contido na model.
+   *
+   * @param value any
+   */
+  writeValue(value: any): void {
+    this.el.nativeElement.value = value;
   }
 }
