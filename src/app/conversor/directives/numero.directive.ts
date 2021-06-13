ea49dc7892933ffeacf864879c39c4e664f34a8a
anderson.antunes <andersonlantunes@gmail.com>
2021-06-13 15:53:33 -0300

    Implementing number validation directive

--- src/app/conversor/directives/numero.directive.ts
+++ src/app/conversor/directives/numero.directive.ts
@@ -1,8 +1,28 @@
-import { Directive } from '@angular/core';
+import { Directive, HostListener, ElementRef } from '@angular/core';
+import {} from '@angular/forms';
 
 @Directive({
   selector: '[numero]',
 })
 export class NumeroDirective {
   constructor() {}
+
+  /**
+   * Implementa evento de keyup para o elemento da diretiva.
+   *
+   * @param $event any
+   */
+  @HostListener('keyup', ['$event']) onKeyUp($event: any) {
+    let valor = $event.target.value;
+    const posDecimais = valor.indexOf('.');
+
+    valor = valor.replace(/[\D]/g, '');
+
+    if (posDecimais > 0) {
+      valor = valor.substr(0, posDecimais) + '.' + valor.substr(posDecimais);
+    }
+
+    $event.target.value = valor;
+    // this.onChange(valor);
+  }
 }
