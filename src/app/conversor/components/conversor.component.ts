fe59d8b88de5f7af0630ae61f193e630377b62b9
anderson.antunes <andersonlantunes@gmail.com>
2021-06-13 14:51:31 -0300

    Implementing currency convert form

--- src/app/conversor/components/conversor.component.ts
+++ src/app/conversor/components/conversor.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { Moeda, Conversao, ConversaoResponse } from '../models';
+import { MoedaService, ConversorService } from '../services';
 
 @Component({
   selector: 'app-conversor',
   templateUrl: './conversor.component.html',
-  styleUrls: ['./conversor.component.css']
+  styleUrls: ['./conversor.component.css'],
 })
 export class ConversorComponent implements OnInit {
+  @ViewChild('conversaoForm', { static: true }) conversaoForm: NgForm;
 
-  constructor() { }
+  moedas: Moeda[];
+  conversao: Conversao;
+  possuiErro: boolean;
+  conversaoResponse: ConversaoResponse;
+
+  constructor(
+    private moedaService: MoedaService,
+    private conversorService: ConversorService
+  ) {}
 
   ngOnInit(): void {
+    this.moedas = this.moedaService.listarTodas();
+    this.init();
   }
 
+  init(): void {
+    this.conversao = new Conversao('USD', 'BRL', null);
+    this.possuiErro = false;
+  }
+
+  converter(): void {
+    if (this.conversaoForm.form.valid) {
+      alert('Convertendo: ' + JSON.stringify(this.conversao));
+    }
+  }
 }
